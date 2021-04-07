cb410db4ea9bbaba9f791ed4fc811b47f38a59ca
Kornél <stefankornel02@gmail.com>
2021-04-07 20:06:36 +0200

    Empty projects

---
+++ backend/src/main/java/hu/hazizz/khn2021/Khn2021Application.java
@@ -0,0 +1,13 @@
+package hu.hazizz.khn2021;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Khn2021Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Khn2021Application.class, args);
+	}
+
+}
---
+++ backend/src/test/java/hu/hazizz/khn2021/Khn2021ApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.hazizz.khn2021;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Khn2021ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
